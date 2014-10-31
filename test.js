c7ce48d39c2f46ce8d3c63eb4c3312a3f5a37ae9
James Costian <james@jamescostian.com>
2014-10-31 14:23:49 -0500

    Add 2 tests

---
+++ test.js
@@ -0,0 +1,30 @@
+var assert = require('assert')
+var util = require('util')
+var fs = require('fs')
+
+var converter = require('./index.js')
+
+describe('converter', function () {
+	it('should work if promises are used', function (done) {
+		converter(fs.createReadStream('./.jshintrc')).then(function (data) {
+			assert.equal(data, fs.readFileSync('./.jshintrc').toString(), 'the stream and the actual string matched')
+			done()
+		}).error(function (err) {
+			assert(false, 'ERROR!')
+			throw err
+			done()
+		})
+	})
+	it('should work if callbacks are used', function (done) {
+		converter(fs.createReadStream('./.jshintrc'), function (err, data) {
+			if (err) {
+				assert(false, 'ERROR!')
+				throw err
+			}
+			else {
+				assert.equal(data, fs.readFileSync('./.jshintrc').toString(), 'the stream and the actual string matched')
+			}
+			done()
+		})
+	})
+})
