f6ecb30c7bd47c8a0a888da5e84833a13c9db48d
James Costian <james@jamescostian.com>
2015-03-18 17:36:42 -0500

    Tests for both .jshintrc and .gitignore

--- test/test.js
+++ test/test.js
@@ -3,22 +3,32 @@ var fs = require('fs')
 
 var converter = require('../index.js')
 
+// List of files to be tested
+var files = ['./.jshintrc', './.gitignore']
+// .jshintrc is ~1K, .gitignore is super tiny
+
 test('promises', function (t) {
-	t.plan(1)
-	converter(fs.createReadStream('./.jshintrc')).then(function (data) {
-		t.equal(data, fs.readFileSync('./.jshintrc').toString(), 'the stream and the actual string match')
-	}).error(function (err) {
-		t.fail(err)
+	t.plan(2)
+
+	files.forEach(function (file) {
+		converter(fs.createReadStream(file)).then(function (data) {
+			t.equal(data, fs.readFileSync(file).toString(), 'the stream and the actual string match (' + file + ')')
+		}).error(function (err) {
+			t.fail(err)
+		})
 	})
 })
 test('callbacks', function (t) {
-	t.plan(1)
-	converter(fs.createReadStream('./.jshintrc'), function (err, data) {
-		if (err) {
-			t.fail(err)
-		}
-		else {
-			t.equal(data, fs.readFileSync('./.jshintrc').toString(), 'the stream and the actual string match')
-		}
+	t.plan(2)
+
+	files.forEach(function (file) {
+		converter(fs.createReadStream(file), function (err, data) {
+			if (err) {
+				t.fail(err)
+			}
+			else {
+				t.equal(data, fs.readFileSync(file).toString(), 'the stream and the actual string match (' + file + ')')
+			}
+		})
 	})
 })
